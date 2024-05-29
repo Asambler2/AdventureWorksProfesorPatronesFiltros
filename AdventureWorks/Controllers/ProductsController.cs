5a16f8e97ae21138d91d100176e7489024a9bd41
Asambler2 <chema508@hotmail.com>
2024-05-29 12:58:30 +0200

    Agregar archivos de proyecto.

---
+++ AdventureWorks/Controllers/ProductsController.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AdventureWorks.Models;
+using AdventureWorks.Services;
+
+namespace AdventureWorks.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AdventureWorks2016Context _context;
+        private readonly ISpecificacionFactory _factoria;
+
+        public ProductsController(AdventureWorks2016Context context,
+            ISpecificacionFactory factoria)
+        {
+            _context = context;
+            _factoria = factoria;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index(string Filtro)
+        {
+
+            var adventureWorks2016Context = _context.Products.Include(p => p.ProductModel).Include(p => p.ProductSubcategory).Include(p => p.SizeUnitMeasureCodeNavigation).Include(p => p.WeightUnitMeasureCodeNavigation);
+            var consulta = _context.Products;
+          
+            switch (Filtro)
+            {
+                case "Sin":
+                    ViewBag.titulo = "Sin Filtro";
+                    return View(await adventureWorks2016Context.ToListAsync());
+                case "F1":
+                    ViewBag.titulo = "1. Sacar los productos de color rojo o verde (red or green), ordenados por SafetyStockLevel de forma ascendente para todo aquellos productos que tengan un ListPrice superior a 1.";
+                    var elemento1 = _factoria.dameInstancia(EnumeracionEjercicios.Ejercicio3);
+                    var consultaFinal1 = (elemento1 as IProductoQuery).dameProductos(consulta);
+                    return View(consultaFinal1);
+                case "F2":
+                    ViewBag.titulo = "2. Sacar los productos de color rojo, ordenados por nombre (name), eliminar los productos de la subcategoria 2 y aquellos que terminen en vocal o en una x.";
+                    var elemento2 = _factoria.dameInstancia(EnumeracionEjercicios.Ejercicio3);
+                    var consultaFinal2 = (elemento2 as IProductoQuery).dameProductos(consulta);
+                    return View(consultaFinal2);
+                case "F3":
+                    ViewBag.titulo = "3. Sacar los productos ordenados por SellStartDate y luego por Color, quiero sólo aquellos que comiencen por a,b o c o que contengan una e en el nombre(name)";
+                    var elemento3 = _factoria.dameInstancia(EnumeracionEjercicios.Ejercicio3);
+                    var consultaFina3 = (elemento3 as IProductoQuery).dameProductos(consulta);
+                    return View(consultaFina3);
+                case "F4":
+                    ViewBag.titulo = "4. Crea una nueva consulta con productos que comiencen por a,b ó c, tenga color rojo, verde o azul, tenga un listprice mayor a 2 y este ordenado por nombre";
+                    var elemento4 = _factoria.dameInstancia(EnumeracionEjercicios.Ejercicio4);
+                    var consultaFina4 = (elemento4 as IProductoQuery).dameProductos(consulta);
+                    return View(consultaFina4);
+
+            }
+            return View(adventureWorks2016Context);
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.ProductModel)
+                .Include(p => p.ProductSubcategory)
+                .Include(p => p.SizeUnitMeasureCodeNavigation)
+                .Include(p => p.WeightUnitMeasureCodeNavigation)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductModelId"] = new SelectList(_context.ProductModels, "ProductModelId", "ProductModelId");
+            ViewData["ProductSubcategoryId"] = new SelectList(_context.ProductSubcategories, "ProductSubcategoryId", "ProductSubcategoryId");
+            ViewData["SizeUnitMeasureCode"] = new SelectList(_context.UnitMeasures, "UnitMeasureCode", "UnitMeasureCode");
+            ViewData["WeightUnitMeasureCode"] = new SelectList(_context.UnitMeasures, "UnitMeasureCode", "UnitMeasureCode");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Name,ProductNumber,MakeFlag,FinishedGoodsFlag,Color,SafetyStockLevel,ReorderPoint,StandardCost,ListPrice,SizeUnitMeasureCode,WeightUnitMeasureCode,Weight,DaysToManufacture,ProductLine,Class,Style,ProductSubcategoryId,ProductModelId,SellStartDate,SellEndDate,DiscontinuedDate,Rowguid,ModifiedDate")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductModelId"] = new SelectList(_context.ProductModels, "ProductModelId", "ProductModelId", product.ProductModelId);
+            ViewData["ProductSubcategoryId"] = new SelectList(_context.ProductSubcategories, "ProductSubcategoryId", "ProductSubcategoryId", product.ProductSubcategoryId);
+            ViewData["SizeUnitMeasureCode"] = new SelectList(_context.UnitMeasures, "UnitMeasureCode", "UnitMeasureCode", product.SizeUnitMeasureCode);
+            ViewData["WeightUnitMeasureCode"] = new SelectList(_context.UnitMeasures, "UnitMeasureCode", "UnitMeasureCode", product.WeightUnitMeasureCode);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductModelId"] = new SelectList(_context.ProductModels, "ProductModelId", "ProductModelId", product.ProductModelId);
+            ViewData["ProductSubcategoryId"] = new SelectList(_context.ProductSubcategories, "ProductSubcategoryId", "ProductSubcategoryId", product.ProductSubcategoryId);
+            ViewData["SizeUnitMeasureCode"] = new SelectList(_context.UnitMeasures, "UnitMeasureCode", "UnitMeasureCode", product.SizeUnitMeasureCode);
+            ViewData["WeightUnitMeasureCode"] = new SelectList(_context.UnitMeasures, "UnitMeasureCode", "UnitMeasureCode", product.WeightUnitMeasureCode);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Name,ProductNumber,MakeFlag,FinishedGoodsFlag,Color,SafetyStockLevel,ReorderPoint,StandardCost,ListPrice,SizeUnitMeasureCode,WeightUnitMeasureCode,Weight,DaysToManufacture,ProductLine,Class,Style,ProductSubcategoryId,ProductModelId,SellStartDate,SellEndDate,DiscontinuedDate,Rowguid,ModifiedDate")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductModelId"] = new SelectList(_context.ProductModels, "ProductModelId", "ProductModelId", product.ProductModelId);
+            ViewData["ProductSubcategoryId"] = new SelectList(_context.ProductSubcategories, "ProductSubcategoryId", "ProductSubcategoryId", product.ProductSubcategoryId);
+            ViewData["SizeUnitMeasureCode"] = new SelectList(_context.UnitMeasures, "UnitMeasureCode", "UnitMeasureCode", product.SizeUnitMeasureCode);
+            ViewData["WeightUnitMeasureCode"] = new SelectList(_context.UnitMeasures, "UnitMeasureCode", "UnitMeasureCode", product.WeightUnitMeasureCode);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.ProductModel)
+                .Include(p => p.ProductSubcategory)
+                .Include(p => p.SizeUnitMeasureCodeNavigation)
+                .Include(p => p.WeightUnitMeasureCodeNavigation)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
---
+++ AdventureWorks/Services/Ejercicio04Builder.cs
@@ -0,0 +1,43 @@
+﻿using AdventureWorks.Models;
+
+namespace AdventureWorks.Services
+{
+    public class Ejercicio04Builder : IProductSpecification, IProductoQuery
+    {
+        private IProductSpecification especificacion;
+
+        public Ejercicio04Builder()
+        {
+            IProductSpecification PorInicio = new NameComienzaSpecification()
+            {
+                letras = ["A", "B", "C"]
+            };
+            IProductSpecification PorColor = new ProductColorSpecification()
+            {
+                colores = ["RED", "BLACK", "BLUE"]
+            };
+            especificacion = new AndSpecification()
+            {
+                Spec1 = PorInicio,
+                Spec2 = PorColor
+            };
+            IProductSpecification PorListPrice = new ListPriceSpecification()
+            {
+                Price = 2
+            };
+            especificacion = new AndSpecification()
+            {
+                Spec1 = especificacion,
+                Spec2 = PorListPrice
+            };
+        }
+        public IEnumerable<Product> dameProductos(IEnumerable<Product> products)
+        {
+            return products.Where(x => especificacion.isValid(x)).OrderBy(x => x.Name);
+        }
+        public bool isValid(Product _producto)
+        {
+            return especificacion.isValid(_producto);
+        }
+    }
+}
